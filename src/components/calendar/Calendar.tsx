4015ddfcfcba9ef9b52ef5c963719438edc7cdfd
Alex Kio <alexandermartelkio@gmail.com>
2021-10-04 16:09:25 -0400

    Add styling to Calendar component

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -14,12 +14,16 @@ export const Calendar: React.FC<CalendarProps> = ({ calendar, addToCalendar }) =
   })
 
   return (
-    <section className='poss'>
-      <section className='possBlurb'>
-        <h2>Your calendar</h2>
-        <p>Click on an hour to delete it from your calendar</p>
-      </section>
-      <section className='cards'>
+    <section className='flex flex-col items-center my-16'>
+      <h2 className='text-3xl capitalize'>Your calendar</h2>
+      <p className="mt-6 text-xl">Click on an hour to delete it from your calendar</p>
+      <section className='p-8
+        grid gap-8 w-11/12
+        sm:grid-cols-2 sm:gap-6 sm:w-auto
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-6
+      '>
         {hourCards}
       </section>
     </section>
