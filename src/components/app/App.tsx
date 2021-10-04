c3c04dd97ca69926848f5f6228de46ff9a0f732e
Alex Kio <alexandermartelkio@gmail.com>
2021-10-04 16:03:14 -0400

    Make header sticky

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -77,9 +77,9 @@ export const App = () => {
 
   return (
     <>
-      <header className="bg-gray-200 shadow-md">
+      <header className="bg-gray-200 shadow-md sticky top-0 z-50">
         <Link to='/'>
-          <h1 className="text-4xl sm:text-7xl text-center p-4 sm:p-8 transition duration-300 ease-in-out hover:text-purple-800">Can I go outside?</h1>
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center p-4 sm:p-8 transition duration-300 ease-in-out hover:text-purple-800">Can I go outside?</h1>
         </Link>
       </header>
       <main>
