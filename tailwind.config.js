784b457896a41d75f4ea3c341e932bd36583f9e9
Alex Kio <alexandermartelkio@gmail.com>
2021-10-04 15:49:24 -0400

    Adjust responsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '550px',
+      'md': '810px',
+      'lg': '1200px',
+      'xl': '1600px'
+    },
     extend: {},
   },
   variants: {
