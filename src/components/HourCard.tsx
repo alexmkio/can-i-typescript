bb67b36caf195e6eb495c8525aa641ccbbcfe978
Alex Kio <alexandermartelkio@gmail.com>
2021-10-06 17:48:56 -0400

    Move rows to HourCard component

--- src/components/HourCard.tsx
+++ src/components/HourCard.tsx
@@ -1,62 +1,23 @@
 import React from 'react';
 import { HourProps } from '../interfaces/index';
 import { calendar } from '../utils/time'
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
 
 export const HourCard: React.FC<HourProps> = ({ hour, addToCalendar }) => {
   let month = calendar.months.find(month => month.number === hour.month)?.name
   let time = calendar.hours.find(thisHour => thisHour.number === hour.hour)?.name
-  let icon = (hour.inCalendar ? 'remove' : 'add')
 
   return (
-    <article className='flex flex-col items-center border p-6 rounded-2xl shadow-md bg-gray-50 transition duration-300 ease-in-out transform hover:bg-gray-100 hover:scale-105'>
-      <label>
-        <button className="material-icons-outlined md-24 bg-yellow-300 rounded-full shadow-md p-3 transition duration-500 ease-in-out transform hover:scale-125"
-          onClick={() => addToCalendar(hour)}
-        >
-          {icon}
-        </button>
-      </label>
-
-      <dl className="flex flex-col items-center text-center uppercase">
-        <dt className="mt-4">Date</dt>
-        <dd className="text-xl">{time} {month} {hour.day}</dd>
-
-        <dt className="mt-4">Temperature</dt>
-        <dd className="text-xl">{hour.temperature} <span>&#8457;</span></dd>
-
-        <dt className="mt-4">Wind Speed</dt>
-        <dd className="text-xl">{hour.windSpeed} mph</dd>
-
-        <dt className="mt-4">Probability of Precipitation</dt>
-        <dd className="text-xl">{hour.precipProb}%</dd>
-      </dl>
-    </article>
+    <TableRow
+      key={`${hour.month}${hour.day}${hour.hour}`}
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell component="th" scope="row">{month} {hour.day}</TableCell>
+      <TableCell align="right">{time}</TableCell>
+      <TableCell align="right">{hour.temperature}</TableCell>
+      <TableCell align="right">{hour.windSpeed}</TableCell>
+      <TableCell align="right">{hour.precipProb}</TableCell>
+    </TableRow>
   )
 }
-
-//   return (
-//     <article className='flex flex-col items-center border p-6 rounded-2xl shadow-md bg-gray-50 transition duration-300 ease-in-out transform hover:bg-gray-100 hover:scale-105'>
-//       <label>
-//         <button className="material-icons-outlined md-24 bg-yellow-300 rounded-full shadow-md p-3 transition duration-500 ease-in-out transform hover:scale-125"
-//           onClick={() => addToCalendar(hour)}
-//         >
-//           {icon}
-//         </button>
-//       </label>
-
-//       <dl className="flex flex-col items-center text-center uppercase">
-//         <dt className="mt-4">Date</dt>
-//         <dd className="text-xl">{time} {month} {hour.day}</dd>
-
-//         <dt className="mt-4">Temperature</dt>
-//         <dd className="text-xl">{hour.temperature} <span>&#8457;</span></dd>
-
-//         <dt className="mt-4">Wind Speed</dt>
-//         <dd className="text-xl">{hour.windSpeed} mph</dd>
-
-//         <dt className="mt-4">Probability of Precipitation</dt>
-//         <dd className="text-xl">{hour.precipProb}%</dd>
-//       </dl>
-//     </article>
-//   )
-// }
