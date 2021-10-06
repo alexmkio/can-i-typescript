598ee88a0c7a10bfaaa6ba8b5d1b373b4dce8a10
Alex Kio <alexandermartelkio@gmail.com>
2021-10-05 20:41:36 -0400

    Make Header smaller

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="bg-gray-200 shadow-md sticky top-0 z-50">
       <Link to='/'>
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center p-4 sm:p-8 transition duration-300 ease-in-out hover:text-purple-800">Can I go outside?</h1>
+        <h1 className="text-4xl md:text-5xl text-center p-4 md:p-6 transition duration-300 ease-in-out hover:text-purple-800">Can I go outside?</h1>
       </Link>
     </header>
   )
