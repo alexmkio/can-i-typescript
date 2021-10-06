d8712241015ee9dab6ec15150c07218e7768d7ab
Alex Kio <alexandermartelkio@gmail.com>
2021-10-05 20:42:02 -0400

    Make Header's background lighter

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <header className="bg-gray-200 shadow-md sticky top-0 z-50">
+    <header className="bg-gray-100 shadow-md sticky top-0 z-50">
       <Link to='/'>
         <h1 className="text-4xl md:text-5xl text-center p-4 md:p-6 transition duration-300 ease-in-out hover:text-purple-800">Can I go outside?</h1>
       </Link>
