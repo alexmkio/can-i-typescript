5dfe090eb9519da524d5d5739fecf133828e19de
Alex Kio <alexandermartelkio@gmail.com>
2021-10-08 16:03:30 -0400

    Register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './components/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const router = <HashRouter> <App /> </HashRouter>;
 
 ReactDOM.render(router, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
